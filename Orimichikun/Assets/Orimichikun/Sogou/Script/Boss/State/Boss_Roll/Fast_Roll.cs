0c8a2dcf807e876929793605c5c34bf0252dec35
sogotoya <Loizacs25@gmail.com>
2025-11-10 00:07:40 +0900

    回転移動処理Fast完成

---
+++ Orimichikun/Assets/Orimichikun/Sogou/Script/Boss/State/Boss_Roll/Fast_Roll.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Fast_Roll : MonoBehaviour
+{
+    [SerializeField]
+    [Header("右の移動ポジション(Pointオブジェクトの子をアタッチ)")]
+    Transform m_Right;
+    [SerializeField]
+    [Header("左の移動ポジション(Pointオブジェクトの子をアタッチ)")]
+    Transform m_Left;
+    [SerializeField]
+    [Header("移動スピード")]
+    float m_MoveSpeed = 1f;
+    [SerializeField]
+    [Header("左右の方向")]
+    int m_Direction = -1;
+
+    //何回右の地点に返ってきたかのカウント
+    int m_Cnt = 0;
+
+
+    /// <summary>
+    /// ボスの回転移動攻撃
+    /// </summary>
+    public void NormalRoll(GameObject obj)
+    {
+        if(m_Cnt!=2)
+        {
+            //現在の場所の更新
+            Vector3 pos= obj.transform.position;
+            //移動処理
+            pos.x += m_MoveSpeed * Time.deltaTime * m_Direction;
+
+            if(pos.x >= m_Right.position.x)
+            {
+                m_Direction= - 1;
+                m_Cnt++;
+            }
+            if(pos.x <= m_Left.position.x)
+            {
+                m_Direction = 1;
+            }
+            //座標更新
+            obj.transform.position = pos;
+        }
+
+    }
+}
