86bddc66e180f06282d5c49d3df7eab65e0418f2
sogotoya <Loizacs25@gmail.com>
2025-11-18 23:10:39 +0900

    コート追加

--- Orimichikun/Assets/Orimichikun/Sogou/Script/Boss/State/Boss_Kobun/SpownWarning.cs
+++ Orimichikun/Assets/Orimichikun/Sogou/Script/Boss/State/Boss_Kobun/SpownWarning.cs
@@ -1,18 +1,34 @@
+//スポーンする場所に危険マークを表示させるスクリプト
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpownWarning : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    [Header("危険マークオブジェクト")]
+    GameObject m_WarningObj;
+
+    [SerializeField]
+    [Header("生成場所")]
+    Transform[] m_Position;
+    [Tooltip("生成場所に生成したオブジェクトのリスト")]
+    List<GameObject> m_WarnObjList;
+    private void Start()
     {
-        
+        if(m_WarningObj==null)
+        {
+            Debug.LogError($"{nameof(m_WarningObj)}アタッチしてください",this);
+        }
+        for(int i = 0; i < m_Position.Length; i++)
+        {
+            //m_WarnObjList=Instantiate(m_WarningObj, m_Position[i].position, m_Position[i].rotation);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnWarning()
     {
-        
+
     }
 }
